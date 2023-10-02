3a2fce86c8e0a4179383fa70e48a1842811f5117
Qianqian <yvonnezhang014@gmail.com>
2023-10-01 20:21:38 -0400

    hard coded signal resoluation & symbol seperation

--- Tx_sim/PAM_4.sv
+++ Tx_sim/PAM_4.sv
@@ -1,37 +1,57 @@
+`timescale 1ns / 1ps
 
 module pam_4_encode(
-        input clk,
-        input rstn,
-        parameter SIGNAL_RESOLUTION = 8,
-        parameter SYMBOL_SEPERATION = 32,
-        input symbol_in[1:0],
-        input symbol_valid,
-        output voltage_level [SIGNAL_RESOLUTION-1:0], //128 to -127 as signed int
-        output reg signal_out_valid =0
-);
-//code goes here:
+    parameter SIGNAL_RESOLUTION = 8,
+    parameter SYMBOL_SEPERATION = 56,
+    input clk,
+    input rstn,
+    input [1:0] symbol_in,
+    input symbol_in_valid,
+    output reg [SIGNAL_RESOLUTION-1:0] voltage_level_out, //128 to -127 as signed int
+    output reg voltage_level_out_valid = 0);
+
     always @ (posedge clk) begin
-        if(rstn == 'b0) begin
-            voltage_level <= 'b0;
-            signal_out_valid <= 'b0;
-            count <= 'b0;
-            map_valid <= 'b0;
+        if (!rstn) begin
+            voltage_level_out <= 0;
+            voltage_level_out_valid <= 0;
+        end else begin
+            if (symbol_in_valid) begin
+                case(symbol_in)
+                    2'b00: voltage_level_out <= -'d84;
+                    2'b01: voltage_level_out <= -'d28;
+                    2'b10: voltage_level_out <= 'd28;
+                    2'b11: voltage_level_out <= 'd84;
+                endcase
+                voltage_level_out_valid <= 1;
+            end
         end
-        //interleaving:
-        switch (symbol_valid) begin
-            // shift valid data into sr
-            2'b00: begin
-                voltage_level = 'd48;
-                signal_out_valid = 'b1;
-            end
-            2'b01: begin
-            end
-            2'b10: begin
-            end
-            2'b11: begin
-            end
+    end
+endmodule
 
+module pam_4_decode(
+    parameter SIGNAL_RESOLUTION = 8,
+    parameter SYMBOL_SEPERATION = 56,
+    input clk,
+    input rstn,
+    input [SIGNAL_RESOLUTION-1:0] voltage_level_in, 
+    input voltage_level_in_valid,
+    output reg [1:0] symbol_out,
+    output reg symbol_out_valid = 0);
+
+    always @ (posedge clk) begin
+        if (!rstn) begin
+            symbol_out <= 0;
+            symbol_out_valid <= 0;
+        end else begin
+            if (voltage_level_in_valid) begin
+                case(voltage_level_in)
+                    -'d84: voltage_level_out <= 2'b00;
+                    -'d28: voltage_level_out <= 2'b01;
+                    'd28: voltage_level_out <= 2'b10;
+                    'd84: voltage_level_out <= 2'b11;
+                endcase
+                symbol_out_valid <= 1;
+            end
         end
-
     end
 endmodule
