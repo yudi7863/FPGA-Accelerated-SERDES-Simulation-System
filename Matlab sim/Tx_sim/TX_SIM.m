49c2dc32de296568afd227706c936fd2f75d9436
Yudi  Wang <wangyud6@p137.ecf.utoronto.ca>
2023-10-03 23:53:44 -0400

    ini files for TX matlab -> have questions!

---
+++ Matlab sim/Tx_sim/TX_SIM.m
@@ -0,0 +1,43 @@
+clc
+clear
+order = 31;
+num_data = 8;
+%SEED = [1,1,0,1,0,0,0,1,0,1,0,1,1,0,1,0,0,1,0,0,1,0,1,0,0,0,1,1,1,1,1];
+SEED = [1,1,1,1,1,0,0,0,1,0,1,0,0,1,0,0,1,0,1,1,0,1,0,1,0,0,0,1,0,1,1]; %reverse???
+prbs_length = 31;
+%generating random data
+
+%c_randomdata =zeros(num_data,data_length);
+c_randomdata = prbs(order, num_data,SEED);
+
+%passing data to grey encoder:
+gray_data = zeros(ceil(double(num_data)/2.0),2);
+num_data_half = ceil(double(num_data)/2.0);
+shifting_reg = [0,0];
+count = 0;
+grey_count = 1;
+for i = 1:num_data
+    shifting_reg(1) = shifting_reg(2);
+    shifting_reg(2) = c_randomdata(i);
+    count = count + 1;
+    if(count == 2)
+        count = 0;
+        gray_data(grey_count,:) = gray_encoder(shifting_reg);
+        grey_count = grey_count + 1;
+    end
+    if(grey_count > num_data_half)
+        break;
+    end
+end
+
+PAM4_out = zeros(1,num_data_half);
+
+%%passing through pam-4
+for i = 1:num_data_half
+    PAM4_out(i) = pam4_TX(gray_data(i,:));
+end
+
+c_randomdata
+gray_data
+PAM4_out
+
---
+++ Matlab sim/Tx_sim/pam4_TX.m
@@ -0,0 +1,14 @@
+function out_data = pam4_TX(in_data)
+    if(in_data == [0,0]) 
+        out_data = -84;
+    elseif(in_data == [1,0]) 
+        out_data = -28; %[lsb,msb] v: []
+    elseif(in_data == [0,1]) %%have question here!!!!!!!
+        out_data = 84;
+    elseif(in_data == [1,1])
+        out_data = 28;
+    else
+        disp('wrong value')
+        %out_data = [-1,-1];
+    end
+end
