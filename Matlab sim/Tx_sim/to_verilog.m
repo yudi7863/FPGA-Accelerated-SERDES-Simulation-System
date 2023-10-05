2a32734d70cf1dfa71563d330f513eb9b432950d
Yudi  Wang <wangyud6@p137.ecf.utoronto.ca>
2023-10-04 21:47:34 -0400

    correct mem file added - in binary

--- Matlab sim/Tx_sim/to_verilog.m
+++ Matlab sim/Tx_sim/to_verilog.m
@@ -3,7 +3,22 @@ function  output_file = to_verilog(in_data,filename,size)
         fileID = fopen(filename,'w');
 
     for i = 1: size
-       fprintf(fileID,"%1d ", in_data(i));
+        switch in_data(i)
+            case -84
+                binVal = [1,0,1,0,1,1,0,0];
+            case -28
+                binVal = [1,1,1,0,0,1,0,0];
+            case 28
+                binVal = [0,0,0,1,1,1,0,0];
+            case 84
+                binVal = [0,1,0,1,0,1,0,0];
+            otherwise
+                binVal = [1,1,1,1,1,1,1,1];
+        end
+        for j = 1:8
+            fprintf(fileID,"%1d", binVal(j));
+        end
+        fprintf(fileID,"\n");
     end
     output_file = fileID;
 end
