75a4d9bec862beae61a1c15b2979bbd0dec7120d
Qianqian <yvonnezhang014@gmail.com>
2023-11-12 17:57:17 -0500

    Added decision feedback equializer testbench

---
+++ Verilog_sim/Rx_sim/rx_tb.v
@@ -0,0 +1,74 @@
+`timescale 1ns / 1ps
+
+module dfe_tb;
+    reg clk = 0;
+    reg en = 0;
+    reg rstn = 0;
+
+    // generate data through prbs->grey_code->pam_4
+    // Generate random data
+    wire binary_data;
+    wire binary_data_valid;
+    prbs31 prbs(
+        .clk(clk),
+        .rstn(rstn),
+        .en(en),
+        .data_out(binary_data),
+        .data_out_valid(binary_data_valid));
+    
+    // Generate grey-coded PAM-4 symbols
+    wire [1:0] symbol;
+    wire symbol_valid;
+    grey_encode ge(
+        .clk(clk),
+        .rstn(rstn),
+        .data_in(binary_data),
+	    .data_in_valid(binary_data_valid),
+        .symbol_out(symbol),
+        .symbol_out_valid(symbol_valid));
+
+    // Generate voltage levels
+    wire [7:0] voltage_level;
+    wire voltage_level_valid;
+    pam_4_encode #(.SIGNAL_RESOLUTION(8), .SYMBOL_SEPERATION(56)) pe(
+        .clk(clk),
+        .rstn(rstn),
+        .symbol_in(symbol),
+	    .symbol_in_valid(symbol_valid),
+        .voltage_level_out(voltage_level),
+        .voltage_level_out_valid(voltage_level_valid));
+
+    wire [7:0] voltage_level_isi;
+    wire voltage_level_isi_valid;
+    ISI_channel channel (
+        .clk(clk),
+        .rstn(rstn),
+        .signal_in(voltage_level),
+        .signal_in_valid(voltage_level_valid),
+        .signal_out(voltage_level_isi),
+        .signal_out_valid(voltage_level_isi_valid));
+
+    wire [7:0] voltage_level_dfe;
+    wire voltage_level_dfe_valid;
+    DFE #(.PULSE_RESPONSE_LENGTH(2), .SIGNAL_RESOLUTION(8), .SYMBOL_SEPERATION(56)) rx(
+        .clk(clk),
+        .rstn(rstn),
+        .signal_in(voltage_level_isi),
+        .signal_in_valid(voltage_level_isi_valid),
+        .signal_out(voltage_level_dfe),
+        .signal_out_valid(voltage_level_dfe_valid));
+
+    
+    //setting clock:
+    always #10 clk = ~clk;
+    //starting simulation:
+    initial begin
+        
+        #50 
+        en <= 1;
+        rstn <=1;
+        #2120
+        
+        $finish;
+    end
+endmodule
